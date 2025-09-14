f35990d332d77a8fccfe8f314276818598bda89b
Josh Harris <yesmaileasy@gmail.com>
2025-09-14 01:11:51 +0100

    Added struct to help with different view systems

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -8,20 +8,20 @@ uniform vec2 point; // line passes through origin (0,0) and this point
 #define OFFSET 0.1
 
 void main() {
-    // circle mask
-    float distance = length(pos.xy);
-    if (distance > 1) {
-        discard;
-    }
+	// circle mask
+	float distance = length(pos.xy);
+	if (distance > 1) {
+		discard;
+	}
 
-    vec2 dir = normalize(point);
-    float d = abs(pos.x * dir.y - pos.y * dir.x);
+	vec2 dir = normalize(point);
+	float d = abs(pos.x * dir.y - pos.y * dir.x);
 
 
-    if (d < LINE_THICKNESS && sign(pos.x) == sign(point.x) && sign(pos.y) == sign(point.y)) {
-        fragColour = vec4(0, distance, 0, 1);
-    } else {
-        fragColour = vec4(1, 0, 0, 1);
-    }
+	if (d < LINE_THICKNESS && sign(pos.x) == sign(point.x) && sign(pos.y) == sign(point.y)) {
+		fragColour = vec4(0, distance, 0, 1);
+	} else {
+		fragColour = vec4(1, 0, 0, 1);
+	}
 }
 
