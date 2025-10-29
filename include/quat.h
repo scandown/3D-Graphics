0c7fdd7292868e6a243be9f7e1d943ed1750837f
Josh Harris <yesmaileasy@gmail.com>
2025-10-29 18:22:55 +0000

    Moved quaternion functions from main.c to quat.c

--- include/quat.h
+++ include/quat.h
@@ -1,1 +1,5 @@
 #pragma once
+
+
+void quat_mul(vec4 q1, vec4 q2, vec4 out);
+void get_rotate_quat(float theta, vec3 axis, vec4 out);
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "shader.h"
 #include "spaces.h"
 #include "model.h"
+#include "quat.h"
 //#include "vec3.h"
 //#include "quat.h"
 //
@@ -16,29 +17,6 @@ double to_degrees(double radians) {
     return radians * (180.0 / M_PI);
 }
 
-void quat_mul(vec4 q1, vec4 q2, vec4 out) {
-	float real = q1[0] - (q1[1] * q2[1]) - (q1[2] * q2[2]) - (q1[3] * q2[3]);
-	float i = (q1[0] * q2[1]) + (q2[0] * q1[1]) + (q1[2] * q2[3]) - (q1[3] * q2[2]);
-	float j = (q1[0] * q2[2]) - (q1[1] * q2[3]) + (q2[0] * q1[2]) + (q1[3] * q2[1]);
-	float k = (q1[0] * q2[3]) + (q1[1] * q2[2]) - (q1[2] * q2[1]) + (q2[0] * q1[3]);
-
-	out[0] = real;
-	out[1] = i;
-	out[2] = j;
-	out[3] = k;
-}
-
-
-void get_rotate_quat(float theta, vec3 axis, vec4 out) {
-	float ca = cos(theta/2.0);
-	float sa = sin(theta/2.0);
-
-	glm_normalize(axis);
-
-	vec4 q = {ca, sa * axis[0], sa * axis[1], sa * axis[2]};
-	glm_vec4_copy(q, out);
-	//out = q;
-}
 
 
 
