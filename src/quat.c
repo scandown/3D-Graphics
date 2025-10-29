2b339f19d4cd57f80ece2198de38ebf2cf7b55e4
Josh Harris <yesmaileasy@gmail.com>
2025-10-29 19:31:41 +0000

    Added more functions to quat library

--- src/quat.c
+++ src/quat.c
@@ -23,3 +23,35 @@ void get_rotate_quat(float theta, vec3 axis, vec4 out) {
 	glm_vec4_copy(q, out);
 	//out = q;
 }
+
+void quat_conj(vec4 q1, vec4 out) {
+	glm_vec4_copy((vec4){q1[0], -q1[1], -q1[2], -q1[3]}, out);
+}
+
+float quat_mag(vec4 q1) {
+	return sqrt((q1[0] * q1[0]) + (q1[1] * q1[1]) + (q1[2] * q1[2]) + (q1[3] * q1[3]));
+}
+
+void print_quat(vec4 q1) {
+	printf("q = {%f, %f, %f, %f}\n", q1[0], q1[1], q1[2], q1[3]);
+}
+
+void quat_inverse(vec4 q1, vec4 out) {
+	vec4 q_conj;
+	quat_conj(q1, q_conj);
+
+	vec4 q1_square;
+	quat_mul(q1, q1, q1_square);
+
+	float q1_square_mag = quat_mag(q1_square);
+
+	for (int i = 0; i < 4; i++) {
+		//q_conj[0] / q1_square_mag, q_conj[1] / q1_square_mag, q_conj[2]
+		q_conj[i] /= q1_square_mag;
+	}
+	glm_vec4_copy(q_conj, out);
+}
+
+void quat_exp(vec4 q1, vec4 out) {
+	float s = exp(q1[0]);
+}
