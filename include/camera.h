263efb0d16bc9f22277e8d214b55c4e3a44d8ef3
Josh Harris <yesmaileasy@gmail.com>
2025-11-10 08:32:48 +0000

    Added camera component to deal with camera input, contains function which creates bitmap for input between A-Z

---
+++ include/camera.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+
+typedef struct {
+	vec3 pos;
+	vec3 front;
+	vec3 up;
+	bool pressed;
+	long key;
+} Camera;
+
+void camera_movement(Camera *cam);
+void key_or(int mask, Camera *cam);
+void key_not(int mask, Camera *cam);
+int get_mask(int key, Camera *cam, void (*key_func)(int, Camera *));
