6bbfc7256b71bb81594eebb2e27b92db44c061f8
Josh Harris <yesmaileasy@gmail.com>
2025-11-15 10:30:37 +0000

    Redid input and made conversion from random values for each keys to a new array which is contiguous
    The code now instead of having an array which stores each offset between the different keysets, now has a new array which stores the difference between the values and also the distance between each block of keys.
    
    This allows for the mask to be converted back to the ASCII values but also allows for the keys to be stored in a mask of two longs which in total is 128 bytes.
    This was sort of achieved in the previous commit however it relied on a value in the camera struct which stored which offset you were in.
    
    However the new system allows for the section to be checked without this variable which means the camera won't get stuck if you suddenly press Space after moving with the WASD keys

--- include/camera.h
+++ include/camera.h
@@ -10,11 +10,13 @@ typedef struct {
 	vec3 up;
 	long mask1;
 	long mask2;
-	int bracket;
 	int key;
 } Camera;
 
 void camera_movement(Camera *cam);
-void key_not(int mask_val, int mask, Camera *cam);
-void key_or(int mask_val, int mask, Camera *cam);
-void get_mask(int key, Camera *cam, void (*key_func)(int, int, Camera *));
+void key_not(int mask_val, long mask, Camera *cam);
+void key_or(int mask_val, long mask, Camera *cam);
+void get_mask(int key, Camera *cam, void (*key_func)(int, long, Camera *));
+
+void key_check(int *key);
+void key_check_inv(int *inv_key);
--- src/camera.c
+++ src/camera.c
@@ -1,49 +1,23 @@
 #include "camera.h"
 
-void camera_movement(Camera *cam) {
-	float cameraSpeed = 0.05f;
-	//set this
-	vec3 cameraMove;
-	vec3 cameraRight;
+int Groups[11][2] = {
+	{GLFW_KEY_SPACE, GLFW_KEY_SPACE},
+	{GLFW_KEY_APOSTROPHE, GLFW_KEY_SPACE},
+	{GLFW_KEY_COMMA, GLFW_KEY_APOSTROPHE},
+	{GLFW_KEY_GRAVE_ACCENT, GLFW_KEY_RIGHT_BRACKET},
+	{GLFW_KEY_WORLD_1, GLFW_KEY_GRAVE_ACCENT},
+	{GLFW_KEY_ESCAPE, GLFW_KEY_WORLD_2},
+	{GLFW_KEY_CAPS_LOCK, GLFW_KEY_END},
+	{GLFW_KEY_F1, GLFW_KEY_PAUSE},
+	{GLFW_KEY_KP_0, GLFW_KEY_F25},
+	{GLFW_KEY_LEFT_SHIFT, GLFW_KEY_KP_EQUAL},
+	{GLFW_KEY_LAST, GLFW_KEY_LAST}
+};
+
+int Groups2[11];
 
 
-	int mask = 1;
-	switch (cam->key - cam->bracket){
-		case GLFW_KEY_W:
-			glm_vec3_scale(cam->front, cameraSpeed, cameraMove);
-			glm_vec3_add(cam->pos, cameraMove, cam->pos);
-			break;
-		case GLFW_KEY_A:
-			glm_vec3_cross(cam->front, cam->up, cameraRight);
-			glm_vec3_normalize(cameraRight);
-			glm_vec3_scale(cameraRight, cameraSpeed, cameraRight);
-
-			glm_vec3_sub(cam->pos, cameraRight, cam->pos);
-			break;
-		case GLFW_KEY_S:
-			glm_vec3_scale(cam->front, cameraSpeed, cameraMove);
-			glm_vec3_sub(cam->pos, cameraMove, cam->pos);
-			break;
-		case GLFW_KEY_D:
-			glm_vec3_cross(cam->front, cam->up, cameraRight);
-			glm_vec3_normalize(cameraRight);
-			glm_vec3_scale(cameraRight, cameraSpeed, cameraRight);
-
-			glm_vec3_add(cam->pos, cameraRight, cam->pos);
-			break;
-		case GLFW_KEY_SPACE:
-			cam->pos[1] += cameraSpeed;
-			break;
-		case GLFW_KEY_LEFT_SHIFT:
-			cam->pos[1] -= cameraSpeed;
-			break;
-		default:
-			break;
-	}
-}
-
-
-void key_or(int mask_val, int mask, Camera *cam) {
+void key_or(int mask_val, long mask, Camera *cam) {
 	if (mask_val == 0) {
 		cam->mask1 = cam->mask1 | mask;
 	} else {
@@ -51,7 +25,7 @@ void key_or(int mask_val, int mask, Camera *cam) {
 	}
 }
 
-void key_not(int mask_val, int mask, Camera *cam) {
+void key_not(int mask_val, long mask, Camera *cam) {
 	if (mask_val == 0) {
 		cam->mask1 = cam->mask1 & ~mask;
 	} else {
@@ -59,83 +33,81 @@ void key_not(int mask_val, int mask, Camera *cam) {
 	}
 }
 
-int Groups[16] = {
-	GLFW_KEY_SPACE,
-	GLFW_KEY_SPACE,
-	GLFW_KEY_APOSTROPHE,
-	GLFW_KEY_APOSTROPHE,
-	GLFW_KEY_COMMA,
-	GLFW_KEY_GRAVE_ACCENT,
-	GLFW_KEY_WORLD_1,
-	GLFW_KEY_WORLD_2,
-	GLFW_KEY_ESCAPE,
-	GLFW_KEY_END,
-	GLFW_KEY_CAPS_LOCK,
-	GLFW_KEY_PAUSE,
-	GLFW_KEY_F1,
-	GLFW_KEY_F25,
-	GLFW_KEY_KP_0,
-	GLFW_KEY_LAST
-};
 
-int key_check(int *key) {
-	int diff = 0;
+void key_check(int *key) {
 	int total_diff = 0;
-	for (int start = 0; start < sizeof(Groups) / sizeof(int); start += 2) {
-		int end = start + 1;
+	for (int i = 0; i < 11; i++) {
+		total_diff += Groups[i][0] - Groups[i][1];
 
-		if (start == 0) {
-			diff = 0;
-		} else {
-			diff = Groups[start-1] - Groups[start];
-		}
-		total_diff += diff + 1;
+		Groups2[i] = Groups[i][0] - Groups[i][1];
 
-		if ((*key >= Groups[start]) && (*key <= Groups[end])) {
-			*key += total_diff;
-			return total_diff;
+		if ((*key < Groups[i+1][0]) && (*key >= Groups[i][0])) {
+			*key -= total_diff + 32;
+			*key += i;
 		}
 	}
-	/*
-	if (*key >= a && key < a_end) {
-	} else if (*key >= b_end) {
-		*key -= b;
-		return b;
-	} else if (*key >= c_end) {
-		*key -= c;
-		return c;
-	} else if (*key >= d_end) {
-		*key -= d;
-		return d;
-	} else if (*key >= e_end) {
-		*key -= e;
-		return e;
-	} else if (*key >= f_end) {
-		*key -= f;
-		return f;
-	} else if (*key >= g_end) {
-		*key -= g;
-		return g;
-	} else if (*key >= h_end) {
-		*key -= h;
-		return h;
-	}
-	*/
-	return -1;
 }
 
-void get_mask(int key, Camera *cam, void (*key_func)(int, int, Camera *)) {
-	int mask = 1;
-	cam->bracket = key_check(&key);
+void key_check_inv(int *inv_key) {
+	int total_diff=0;
+	int Groups3[11];
+	for (int i = 0; i < 11; i++) {
+		total_diff+=Groups2[i];
+		int lower_bracket = Groups[i][0] - total_diff + i;
+		Groups3[i] = lower_bracket;
+	}
+
+	total_diff=0;
+	for (int i = 0; i < 11; i++) {
+		total_diff+=Groups2[i];
+		int offset = i;
+		int lower = Groups3[offset] - 32;
+		if (i + 1 < 11) {
+			offset = i+1;
+		}
+		int upper = Groups3[offset] - 32;
+
+
+		if (*inv_key < upper && *inv_key >= lower) {
+			*inv_key = *inv_key + total_diff + 32 - i;
+			break;
+		}
+	}
+}
+
+
+void get_mask(int key, Camera *cam, void (*key_func)(int, long, Camera *)) {
+	long mask = 1;
+	key_check(&key);
 	cam->key = key;
-	printf("%d\n", cam->key - cam->bracket);
-	if (key < sizeof(long)) {
-		int new_key = key;
+	if (cam->key < 64) {
+		long new_key = cam->key;
 		mask = mask << new_key;
 		key_func(0, mask, cam);
-	} else if (key - sizeof(long) < sizeof(long)) {
-		int new_key = key - 64;
+	} else {
+		long new_key = cam->key - 64;
 		mask = mask << new_key;
 		key_func(1, mask, cam);
+	}
+}
+void camera_movement(Camera *cam) {
+	long mask = 1;
+	int amount = 0;
+
+	for (int i = 0; i < 64; i++) {
+		mask = mask << 1;	
+		amount++;
+		int amount_copy = amount;
+
+		if ((cam->mask1 & mask) != 0) {
+			key_check_inv(&amount_copy);
+			printf("%d\n", amount_copy);
+		}
+		if ((cam->mask2 & mask) != 0) {
+			amount_copy += 64;
+			key_check_inv(&amount_copy);
+			printf("%d\n", amount_copy);
+		}
+
 	}
 }
