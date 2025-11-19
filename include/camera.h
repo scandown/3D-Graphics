a2c8d5bed640e38808f25c94f06525f364e83fcc
Josh Harris <yesmaileasy@gmail.com>
2025-11-19 10:56:16 +0000

    Renamed get_mask to set_input_mask

--- include/camera.h
+++ include/camera.h
@@ -23,7 +23,7 @@ typedef struct {
 void camera_movement(Camera *cam);
 void key_not(int mask_val, long mask, Camera *cam);
 void key_or(int mask_val, long mask, Camera *cam);
-void get_mask(int key, Camera *cam, void (*key_func)(int, long, Camera *));
+void set_input_mask(int key, Camera *cam, void (*key_func)(int, long, Camera *));
 
 void key_check(int *key);
 void key_check_inv(int *inv_key);
