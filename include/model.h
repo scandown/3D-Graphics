b475131c932481acc80f77f031cd342fb15ac4b2
Josh Harris <yesmaileasy@gmail.com>
2025-11-21 02:46:48 +0000

    Super scuffed texture loading

--- include/model.h
+++ include/model.h
@@ -8,12 +8,10 @@
 
 
 typedef struct {
+	float *vertices;
 	int vertex_size;
-	int texture_size;
 	int vertex_face_size;
 	int texture_face_size;
-	float *vertices;
-	float *textures;
 	unsigned int *vertex_faces;
 	unsigned int *texture_faces;
 	char *location;
@@ -22,6 +20,7 @@ typedef struct {
 void string_search_vertex_point(int *i,float *array, char *line);
 void string_search_texture_point(int *i,float *array, char *line);
 void string_search_vertex_face(int *i, unsigned int *array, char *line);
+void string_search_texture_face(int *i, unsigned int *array, char *line);
 
 int check_int_equality(int *array1, int array1_length, int *array2, int array2_length);
 int check_float_equality(float *array1, int array1_length, float *array2, int array2_length);
