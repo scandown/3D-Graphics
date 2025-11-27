d57032c84340e151fa62a55cb11dfea09d2723a9
Josh Harris <yesmaileasy@gmail.com>
2025-11-27 14:14:05 +0000

    Redid model load function so that it uses a binary search tree to remove duplicate faces and group them together under one face rather than using two separate indices in the obj file format

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -1,62 +1,99 @@
 #include "binary_tree.h"
 
-BST *createnode(int value) {
+
+BST *createnode(float value, float value2, float linked_number) {
 	BST *result = malloc(sizeof(BST));
 	result->left = NULL;
 	result->right = NULL;
-	result->value = value;
+	result->value.vertex = value;
+	result->value.texture = value2;
+	result->linked = linked_number;
 
 	return result;
 }
 
-bool insertnumber(BST **rootptr, int value) {
+bool insertnumber(BST **rootptr, float value, float value2, float linked_number) {
 	BST *root = *rootptr;
 	if (root == NULL) {
-		*rootptr = createnode(value);
+		*rootptr = createnode(value, value2, linked_number);
 		return true;
 	}
-	if (value == root->value) {
+	if (value == root->value.vertex && value2 == root->value.texture) {
 		return false;
 	}
 
-	if (value < root->value) {
-		return insertnumber(&(root->left), value);	
+	if (value == root->value.vertex) {
+		if (value2 < root->value.texture) {
+			return insertnumber(&(root->left), value, value2, linked_number);
+		}
+		else if (value2 > root->value.texture) {
+			return insertnumber(&(root->right), value, value2, linked_number);
+		}
 	} else {
-		return insertnumber(&(root->right), value);	
+		if (value < root->value.vertex) {
+			return insertnumber(&(root->left), value, value2, linked_number);	
+		} else {
+			// if all else is false then this must be true
+			return insertnumber(&(root->right), value, value2, linked_number);	
+		}
 	}
+
+	return false;
 }
 
-bool getnumber(BST *rootptr, int value) {
+bool getnumber(BST *rootptr, float value, float value2) {
 	if (rootptr == NULL) {
 		return false;
 	}
 
-	if (rootptr->value == value) {
+	if (rootptr->value.vertex == value && rootptr->value.texture == value2) {
 		return true;
 	}
 
-	if (value < rootptr->value) {
-		return getnumber(rootptr->left, value);	
+	if (value == rootptr->value.vertex) {
+		if (value2 < rootptr->value.texture) {
+			return getnumber(rootptr->left, value, value2);
+		}
+		else if (value2 > rootptr->value.texture) {
+			return getnumber(rootptr->right, value, value2);
+		}
 	} else {
-		return getnumber(rootptr->right, value);	
+		if (value < rootptr->value.vertex) {
+			return getnumber(rootptr->left, value, value2);	
+		} else {
+			// if all else is false then this must be true
+			return getnumber(rootptr->right, value, value2);	
+		}
 	}
+
+	return false;
 }
 
-bool free_bst(BST *root) {
+bool free_bst(BST **rootptr) {
+	BST *root = *rootptr;
 	if (root == NULL) {
-		return true;
+		return false;
 	}
 
 	if (root->left != NULL) {
-		free_bst(root->left);
+		free_bst(&root->left);
 		root->left = NULL;
 	}
 
 	if (root->right != NULL) {
-		free_bst(root->right);
+		free_bst(&root->right);
 		root->right = NULL;
 	}
 
 	free(root);
+	*rootptr = NULL;
 	return true;
+}
+
+void print_tree(BST *rootptr) {
+    if (rootptr == NULL) return;
+    
+    print_tree(rootptr->left);
+    printf("Vertex: %f, Texture: %f / Linked: %f\n", rootptr->value.vertex, rootptr->value.texture, rootptr->linked);
+    print_tree(rootptr->right);
 }
