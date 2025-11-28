b793bba0e51ec68179aba2bc6f9dc89be6a66847
Josh Harris <yesmaileasy@gmail.com>
2025-11-28 01:05:04 +0000

    Loading the indices and other vertices plus got BST working, the rendered cube looks dumb though

--- include/binary_tree.h
+++ include/binary_tree.h
@@ -5,20 +5,21 @@
 
 
 typedef struct {
-	float vertex;
-	float texture;
+	int vertex;
+	int texture;
 } Val;
 
 typedef struct bst{
 	Val value;
 	struct bst *left;
 	struct bst *right;
-	float linked;
+	int linked;
 } BST;
 
 
 BST *createnode(float value, float value2, float linked_number);
 bool insertnumber(BST **rootptr, float value, float value2, float linked_number);
 bool getnumber(BST *rootptr, float value, float value2);
+BST *getvalue(BST *rootptr, float value, float value2);
 bool free_bst(BST **rootptr);
 void print_tree(BST *rootptr);
