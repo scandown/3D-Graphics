355661a396833e8622498cda457ccac7bf20d5e8
Josh Harris <yesmaileasy@gmail.com>
2025-12-02 10:10:02 +0000

    Moved texture config into texture setup function

--- src/texture.c
+++ src/texture.c
@@ -22,6 +22,16 @@ unsigned int texture_setup(jmp_buf error, GLenum format, char *name) {
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
 	stbi_image_free(data);
 	
 	return texture;
