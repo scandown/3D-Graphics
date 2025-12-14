551a3b4d719e22136673d1d136a330ceedf6a421
Josh Harris <yesmaileasy@gmail.com>
2025-12-14 01:32:47 +0000

    Added dynamic arrays

--- src_build/nob_configed.c
+++ src_build/nob_configed.c
@@ -29,7 +29,7 @@ int main(void)
     nob_cc_output(&cmd, output_path);
     nob_cc_inputs(&cmd, input_path, "external/lib/glad.c", SRC_FOLDER"shader.c", SRC_FOLDER"spaces.c", SRC_FOLDER"model.c", SRC_FOLDER"quat.c",
 		    SRC_FOLDER"window.c", SRC_FOLDER"camera.c", SRC_FOLDER"state.c", SRC_FOLDER"uniform.c", SRC_FOLDER"texture.c",
-		    SRC_FOLDER"binary_tree.c");
+		    SRC_FOLDER"binary_tree.c", SRC_FOLDER"dynamic_array.c");
     nob_cmd_append(&cmd, "-lglfw3", "-lm", "-ldl", "-lpthread", "-lGL", "-lX11");
     nob_cmd_append(&cmd, "-Wno-unused-variable", "-Wno-unused-parameter");
     nob_cmd_append(&cmd, "-Wno-sign-compare");
