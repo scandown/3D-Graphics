d08a5477e9a975123a7f22a0952bdfcaebdce85f
Josh Harris <yesmaileasy@gmail.com>
2026-01-03 18:19:40 +0000

    Added function to state module which sets up the spaces matrices

--- include/state.h
+++ include/state.h
@@ -1,9 +1,14 @@
 #pragma once
+
+#include <setjmp.h>
+
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "shader.h"
 #include "window.h"
-#include <setjmp.h>
+#include "uniform.h"
+
+typedef struct Uniform Uniform;
 
 typedef struct {
 	unsigned int program;
@@ -11,7 +16,12 @@ typedef struct {
 	unsigned int SCR_HEIGHT;
 	char *title;
 	GLFWwindow *window;
+
+	Uniform projection_uniform;
+	Uniform view_uniform;
+	Uniform model_uniform;
 } State;
 
 
 State setup_state(jmp_buf error, unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT, char *name, char *shader_location);
+void setup_scene(State *game, unsigned int program);
--- src/state.c
+++ src/state.c
@@ -17,3 +17,19 @@ State setup_state(jmp_buf error, unsigned int SCR_WIDTH, unsigned int SCR_HEIGHT
 
 	return state;
 }
+
+
+void setup_scene(State *game, unsigned int program) {
+	mat4 view_matrix;
+	mat4 projection_matrix;
+	mat4 model_matrix;
+	glm_mat4_identity(view_matrix);
+	glm_mat4_identity(model_matrix);
+	glm_mat4_identity(projection_matrix);
+
+	game->view_uniform = uniform_init(program, "view", view_matrix, UNIFORM_MAT4);
+	game->model_uniform = uniform_init(program, "model", model_matrix, UNIFORM_MAT4);
+	game->projection_uniform = uniform_init(program, "projection", projection_matrix, UNIFORM_MAT4);
+	uniform_send(&game->model_uniform);
+	uniform_send(&game->view_uniform);
+}
