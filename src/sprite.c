21f8a24f5f543ec4d9abbc9a6b4cff72446ac302
Josh Harris <yesmaileasy@gmail.com>
2026-01-11 23:17:16 +0000

    Brokey files fix later

--- src/sprite.c
+++ src/sprite.c
@@ -21,3 +21,32 @@ void sprite_draw(Sprite *sprite, unsigned int program, int instance_amount) {
 void sprite_delete(Sprite *sprite) {
 	model_delete_buffers(&sprite->plane);
 }
+
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float)));
+	//glEnableVertexAttribArray(2);
+
+
+void sprite_generate_model(Model *model, int width, int height) {
+	// vertex, uv, normal
+	unsigned int vertices[8 * 4] = {
+		0, 0, 0,	0, 0,	0, 0, 0,
+		1, 0, 0,	0, 0,	0, 0, 0,
+		1, 1, 0,	0, 0,	0, 0, 0,
+		0, 1, 0,	0, 0,	0, 0, 0
+	}
+
+	unsigned int faces[3 * 2] = {
+		0, 2, 1,
+		0, 3, 2
+	}
+
+	memcpy(model->vertices, vertices, 8 * 4);
+	memcpy(model->vertex_faces, faces, 3 * 2);
+	model->vertex_size = 8 * 4;
+	model->vertex_face_size = 3 * 2;
+
+}
