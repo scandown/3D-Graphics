2bcde8aea9853e0e349887e7bf568d84ac9b82d1
Josh Harris <yesmaileasy@gmail.com>
2026-01-22 17:13:29 +0000

    Replaced old input system with much simpler glfwGetKey based method

--- src_build/nob_configed.c
+++ src_build/nob_configed.c
@@ -29,7 +29,7 @@ int main(void)
     nob_cc_output(&cmd, output_path);
     nob_cc_inputs(&cmd, input_path, "external/lib/glad.c", SRC_FOLDER"shader.c", SRC_FOLDER"model.c", SRC_FOLDER"quat.c",
 		    SRC_FOLDER"window.c", SRC_FOLDER"camera.c", SRC_FOLDER"state.c", SRC_FOLDER"uniform.c", SRC_FOLDER"texture.c",
-		    SRC_FOLDER"binary_tree.c", SRC_FOLDER"sprite.c");
+		    SRC_FOLDER"binary_tree.c", SRC_FOLDER"sprite.c", SRC_FOLDER"input.c");
     nob_cmd_append(&cmd, "-lglfw3", "-lm", "-ldl", "-lpthread", "-lGL", "-lX11");
     nob_cmd_append(&cmd, "-Wno-unused-variable", "-Wno-unused-parameter");
     nob_cmd_append(&cmd, "-Wno-sign-compare");
