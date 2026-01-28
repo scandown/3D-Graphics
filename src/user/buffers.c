864853a05395acd179cff427aac2d766fc200b07
Josh Harris <yesmaileasy@gmail.com>
2026-01-28 01:13:04 +0000

    Removed unnecessary functions and merged them into more general functions for model initialisation

--- src/user/buffers.c
+++ src/user/buffers.c
@@ -1,48 +1,6 @@
 #include "user/buffers.h"
 
-void model_send_to_gpu(Model *model) {
-
-	glGenVertexArrays(1, &model->VAO);
-	glGenBuffers(1, &model->VBO);
-	glGenBuffers(1, &model->EBO);
-
-
-	glBindVertexArray(model->VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, model->VBO);
-	glBufferData(GL_ARRAY_BUFFER, model->vertex_size * sizeof(float), model->vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model->EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, model->vertex_face_size * sizeof(unsigned int), model->vertex_faces, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-}
-
-void model_send_to_gpu_instanced(int translation_size; Model *model, vec2 translations[translation_size], vec2 spr_num[translation_size], int translation_size) {
-
-	// move these into less generalised functions
-	// you don't want to hide away the opengl logic
-	//
-	// -- especially in this case as this stuff is volatile and
-	// -- changes drastically depending on the project
-	//
-	// possibly make sprite have its own function which deals with buffer
-	// functions directly and also any other rendering task in the future
-	//
-	//
-	// make src_user folder to have code that the user of this engine
-	// can create / modify for this exact use case
-
-
+void model_send_to_gpu(int translation_size; Model *model, Instance *instance, int translation_size) {
 	glGenVertexArrays(1, &model->VAO);
 	glGenBuffers(1, &model->VBO);
 	glGenBuffers(1, &model->EBO);
@@ -58,11 +16,6 @@ void model_send_to_gpu_instanced(int translation_size; Model *model, vec2 transl
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, model->EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, model->vertex_face_size * sizeof(unsigned int), model->vertex_faces, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ARRAY_BUFFER, model->instance_UV_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * translation_size, translations, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, model->instance_spr_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * translation_size, spr_num, GL_STATIC_DRAW);
 
 
 	glBindBuffer(GL_ARRAY_BUFFER, model->VBO);
@@ -73,15 +26,24 @@ void model_send_to_gpu_instanced(int translation_size; Model *model, vec2 transl
 	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
-	glBindBuffer(GL_ARRAY_BUFFER, model->instance_UV_VBO);
-	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)(0));
-	glVertexAttribDivisor(3, 1);
-	glEnableVertexAttribArray(3);
 
-	glBindBuffer(GL_ARRAY_BUFFER, model->instance_spr_VBO);
-	glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)(0));
-	glVertexAttribDivisor(4, 1);
-	glEnableVertexAttribArray(4);
+	if (instance->is_instanced) {
+		glBindBuffer(GL_ARRAY_BUFFER, model->instance_UV_VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * translation_size, instance->instance_array, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, model->instance_spr_VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * translation_size, instance->spr_num, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ARRAY_BUFFER, model->instance_UV_VBO);
+		glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)(0));
+		glVertexAttribDivisor(3, 1);
+		glEnableVertexAttribArray(3);
+
+		glBindBuffer(GL_ARRAY_BUFFER, model->instance_spr_VBO);
+		glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)(0));
+		glVertexAttribDivisor(4, 1);
+		glEnableVertexAttribArray(4);
+	}
 
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -123,36 +85,22 @@ void model_draw_instanced(Model *model, unsigned int program, unsigned int insta
 	glDrawElementsInstanced(GL_TRIANGLES, model->vertex_face_size, GL_UNSIGNED_INT, 0, instance_amount);
 }
 
-
-void model_init_instanced(int instance_amount; jmp_buf error, Model *model, vec3 pos, char *texture_location,
-		vec2 instance_array[instance_amount], vec2 spr_num[instance_amount], int instance_amount) {
+void model_init(jmp_buf error, Model *model, vec3 pos, char *texture_location, Instance *instance, int instance_amount) {
 
 	unsigned int texture = texture_init(error, GL_RGBA, texture_location);
 
 	mat4 model_matrix;
 	glm_mat4_identity(model_matrix);
 
-	model_send_to_gpu_instanced(model, instance_array, spr_num, instance_amount);
-
+	if (instance->is_instanced) {
+		model_send_to_gpu(model, instance, instance_amount);
+	} else {
+		model_send_to_gpu(model, instance, 0);
+	}
 
 	model->uniform = uniform_set_data(model_matrix, UNIFORM_MAT4);
 	model->x = pos[0];
 	model->y = pos[1];
 	model->z = pos[2];
-	model->texture = texture;
-}
-
-void model_init(jmp_buf error, Model *model, vec3 pos, char *texture_location) {
-	unsigned int texture = texture_init(error, GL_RGBA, texture_location);
-
-	mat4 model_matrix;
-	glm_mat4_identity(model_matrix);
-
-	model_send_to_gpu(model);
-	model->uniform = uniform_set_data(model_matrix, UNIFORM_MAT4);
-	model->x = pos[0];
-	model->y = pos[1];
-	model->z = pos[2];
-
 	model->texture = texture;
 }
